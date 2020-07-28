b880e69a4d4a0e155e0e1785d1c746a00b230257
Diahnelson <diahhgl@gmail.com>
2020-07-29 02:23:07 +0800

    updates to deploy to heroku

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, TemplateRef, Input, ElementRef } from '@angular/core'; 
+  
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
-})
-export class AppComponent {
-  title = 'diahtodo4';
+interface Task{
+    title: string,
+    is_canceled: boolean
+   }
+
+@Component({ 
+    selector: 'app-root', 
+    templateUrl: './app.component.html', 
+    styleUrls: ['./app.component.css'] 
+}) 
+export class AppComponent { 
+  
+    /* An empty array that is responsible 
+       to add a division */
+    public items = []; 
+  
+    /* A two-way binding performed which 
+       pushes text on division */
+    public newTask; 
+  
+  
+    /* When input is empty, it will 
+       not create a new division */
+    public addToList() { 
+        if (this.newTask == '') { 
+        } 
+        else { 
+            this.items.push(this.newTask); 
+            this.newTask = ''; 
+        } 
+    } 
+  
+    /* This function takes to input the 
+       task, that has to be deleted*/
+    public deleteTask(index) { 
+        let do_delete = confirm("Are you sure to delete Work on new e-commerce web app?");
+        if (do_delete){
+          this.items.splice(index, 1);
+       }
+} 
+
+
 }
